bc4b41145b80779a3836832ce4575b7fdeca9043
Natalia Owsiejko <natalia.owsiejko@onet.pl>
2024-10-09 13:59:57 +0200

    do dokonczenia

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,13 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel, kod):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
+        if len(pesel) != 11:
+            self.pesel = "Niepoprawny pesel"
+        else:
+            self.pesel = pesel
+        if kod[0:4] == "PROM_" and len(kod)==8:
+            self.saldo = 50
+        else:
+            self.saldo = 0
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,12 +5,30 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
     imie = 'Dariusz'
     nazwisko = "Januszewski"
-    
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto(self.imie, self.nazwisko)
+        pesel = "12345678944"
+        pierwsze_konto = Konto(self.imie, self.nazwisko, pesel)
         self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, self.nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, pesel, "pesel nie zostal zapisany")
 
-    #tutaj proszę dodawać nowe testy
+
+    def test_zbyt_dlugi_pesel(self):
+        pesel = "123435674647632"
+        pierwsze_konto = Konto(self.imie, self.nazwisko, pesel, "PROM_123")
+        self.assertEqual(pierwsze_konto.pesel, "Niepoprawny pesel", "Pesel nie zostal zapisany")
+    def test_zbyt_krotki_pesel(self):
+        pesel = "1234"
+        pierwsze_konto = Konto(self.imie, self.nazwisko, pesel, "PROM_123")
+        self.assertEqual(pierwsze_konto.pesel, "Niepoprawny pesel", "Pesel nie zostal zapisany")
+    def test_kod_promocyjny(self):
+        pesel =" 12345678901"
+        kod = "PROM_123"
+        pierwsze_konto=Konto(self.imie, self.nazwisko, pesel, kod)
+        self.assertEqual(pierwsze_konto.kod, self.kod, "Promocja nie zostala naliczona")
+
+
+        
+    
